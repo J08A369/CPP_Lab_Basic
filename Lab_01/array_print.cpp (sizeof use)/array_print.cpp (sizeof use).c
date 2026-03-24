ba694510f6ecb974827b883ac650c6f5e12f593b
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-03-25 01:59:00 +0600

    Add files via upload

---
+++ Lab_01/array_print.cpp (sizeof use)/array_print.cpp (sizeof use).c
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[7] = {1,2,3,4,5,6,7};
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i=0; i<size; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
