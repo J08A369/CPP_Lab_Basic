08b5c77b979dc804a87d345b916ef272ae1a539c
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-04-08 05:07:44 +0600

    Add files via upload

---
+++ Lab_01/simple_interest.cpp/simple_interest.cpp
@@ -0,0 +1,12 @@
+
+//Formula: I = P × n × r
+#include <iostream>
+using namespace std;
+
+int main() {
+    double P, n, r, I;
+    cin >> P >> n >> r;
+    I = P * n * r;
+    cout << I;
+    return 0;
+}
