0db7f3dce37a0d804ef7908e0855bdb757d65b21
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-05-04 09:39:01 +0600

    Add files via upload

---
+++ Lab_06/School_Management_System.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class School {
+
+public:
+    int fees;
+
+    School() {
+        cout << "School Started" << endl;
+    }
+
+    void student() {
+        cout << "Regular Student" << endl;
+    }
+
+    void student(string group) {
+        cout << "Group: " << group << endl;
+    }
+
+    School operator + (School s) {
+
+        School temp;
+
+        temp.fees = fees + s.fees;
+
+        return temp;
+    }
+
+    ~School() {
+        cout << "School Closed" << endl;
+    }
+};
+
+int main() {
+
+    School s1, s2;
+
+    s1.fees = 3000;
+    s2.fees = 4000;
+
+    s1.student();
+    s1.student("Science");
+
+    School s3 = s1 + s2;
+
+    cout << "Total Fees: " << s3.fees << endl;
+
+    School *s = new School();
+
+    delete s;
+
+    return 0;
+}
