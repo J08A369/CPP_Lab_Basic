d6b15bbbdd256929de064c3ea583de417eb3e07c
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-08-01 20:36:10 +0600

    Add files via upload

---
+++ Lab 2.0/Bank_Account.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class BankAccount {
+public:
+    double balance;
+
+    void showBalance() {
+        cout << "Balance: " << balance << " Tk";
+    }
+};
+
+int main() {
+    BankAccount b;
+    b.balance = 5000;
+    b.showBalance();
+    return 0;
+}
+
