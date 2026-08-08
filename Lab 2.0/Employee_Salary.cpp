d9710587cdce9087cbcc58224a7696b5d5c380b9
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-08-08 14:37:48 +0600

    Add files via upload

---
+++ Lab 2.0/Employee_Salary.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Employee {
+public:
+    string name;
+    int salary;
+
+    void show() {
+        cout << "Name: " << name << endl;
+        cout << "Salary: " << salary << " Tk";
+    }
+};
+
+int main() {
+    Employee e;
+    e.name = "Rahim";
+    e.salary = 20000;
+    e.show();
+    return 0;
+}
+
+
