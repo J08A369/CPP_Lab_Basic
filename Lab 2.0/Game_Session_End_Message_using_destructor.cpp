65332d4a23845cae2fcdc2af23dcd6c543ae54f4
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-08-11 23:52:56 +0600

    Add files via upload

---
+++ Lab 2.0/Game_Session_End_Message_using_destructor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Game {
+public:
+    Game() {
+        cout << "Game Started!" << endl;
+    }
+    ~Game() {
+        cout << "Game Over!" << endl;
+    }
+};
+
+int main() {
+    Game g;
+    return 0;
+}
+
+
